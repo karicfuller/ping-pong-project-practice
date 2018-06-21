fbae0acaf5f03c02d35bc4ab8f0f0f41d904be15
kari and visetty <kari@>
2018-06-20 20:10:54 -0700

    adds js to the ping-pong webpage

--- Desktop/ping-pong/js/scripts.js
+++ Desktop/ping-pong/js/scripts.js
@@ -1,23 +1,30 @@
+
+
+
+var oneInput = function(numberOneInput){
+  //debugger;
+    if (isNaN(numberOneInput)) {
+    $("#resultNumber").append("<li>" + "Enter a valid Number:" + "</li>");
+  } else if(numberOneInput%15 === 0) {
+    $("#resultNumber").append("<li>" + "ping-pong" + "</li>");
+  } else if(numberOneInput%5 === 0) {
+    $("#resultNumber").append("<li>" + "pong" + "</li>");
+  } else if(numberOneInput%3 === 0) {
+    $("#resultNumber").append("<li>" + "ping" + "</li>");
+  } else if((numberOneInput%15 !== 0) && (numberOneInput%5 !== 0) && (numberOneInput%3 !== 0)) {
+    $("#resultNumber").append("<li>" + numberOneInput + "</li>");
+  } else {
+    $("#resultNumber").append("<li>" + "Enter valid input"+ "</li>");
+  }
+};
+
+
+
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+    //debugger;
     var numberOneInput = $("input#numberOne").val();
-    //console.log(numberOneInput);
-
-      //var result = (numberOneInput/3);
-      //alert(result);
-
-      if (isNaN(numberOneInput)) {
-        alert("enter a number");
-      } else if(numberOneInput%15 === 0) {
-        alert("ping-pong");
-      } else if(numberOneInput%5 === 0) {
-        alert("pong");
-      } else if(numberOneInput%3 === 0) {
-        alert("ping");
-      } else if((numberOneInput%15!== 0) && (numberOneInput%5!== 0) && (numberOneInput%3!== 0)) {
-        alert(numberOneInput);
-      }
-
+    oneInput(numberOneInput);
 
     event.preventDefault();
   });
